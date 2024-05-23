af140ba0a8c1ec72f0a77c6269d36f25866d128a
KumaCatz <tod.kagan@gmail.com>
2024-05-23 17:16:48 -0300

    username table html

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
-app.use(cors())
-app.use(express.static('public'))
+const client = new MongoClient(process.env.DB_URL);
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
+app
+  .route('/api/users')
+  .get((req, res) => {
+    res.json('hey girl');
+  })
+  .post((req, res) => {
+    res.json('hey girl');
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
