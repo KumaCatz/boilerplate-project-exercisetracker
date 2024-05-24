0908748cc99065be17812544803c9ab184825fd0
KumaCatz <tod.kagan@gmail.com>
2024-05-23 22:03:59 -0300

    .

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const client = new MongoClient(process.env.DB_URL);
+const db = client.db("exercise_tracker");
+const user = db.collection("user")
+const exercise = db.collection("exercise")
 const app = express();
 
 app.use(cors());
@@ -17,12 +20,49 @@ app.get('/', (req, res) => {
 
 app
   .route('/api/users')
-  .get((req, res) => {
-    res.json('hey girl');
+  .get(async (req, res) => {
+    const cursor = user.find();
+    const results = await cursor.toArray();
+    res.json(results);
   })
-  .post((req, res) => {
-    res.json('hey girl');
+  .post(async (req, res) => {
+    const { username } = req.body;
+    const userDoc = {
+      username: username,
+    };
+    const result = await user.insertOne(userDoc);
+    res.json({
+      username: username,
+      _id: result.insertedId,
+    });
   });
+
+app.post('/api/users/:id/exercises', async (req, res) => {
+    const {id, desc, dur, date} = req.body;
+
+    const getUser = user.find({_id: new ObjectId(id)})[0];
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    const exerciseDoc = {
+      username: getUser,
+      description: desc,
+      duration: dur,
+      date: date,
+      _id: id
+    }
+
+    const result = await exercise.insertOne(exerciseDoc);
+
+    console.log(result)
+    console.log(user)
+    res.json(exerciseDoc.username)
+  });
+
+app.get('/api/users/:id/logs', (req, res) => {})
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
